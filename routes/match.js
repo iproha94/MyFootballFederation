d9d78b37406fe513b8e2c1f9a21816f582b53ac1
Saboteuur <Saboteuur@yandex.ru>
2016-10-02 21:15:07 +0300

    add match page

--- routes/match.js
+++ routes/match.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var Match = require('../models/match');
+var Team = require('../models/team');
 
 router.get('/:idMatch', function(req, res, next) {
-    Match.findById(req.params.isMatch, function (err, match) {
+    Match.findById(req.params.idMatch, function (err, match) {
         if(err) {
             return next(err);
         }
-        res.render("match", match);
+        
+        Team.findById(match.team1, function (err, team1) {
+            Team.findById(match.team2, function (err, team2) {
+                res.render("match", {
+                    match: match,
+                    team1: team1,
+                    team2: team2
+                });
+            });
+        });
     });
 });
 
