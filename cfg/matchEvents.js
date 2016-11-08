7090e68f72e9b093693c3b297693e9d72279abae
Evgeny-Ivanov <Saboteuur@yandex.ru>
2016-11-08 13:55:51 +0300

    ignore cfg

---
+++ cfg/matchEvents.js
@@ -0,0 +1,41 @@
+module.exports = {
+    MATCH_START : {
+        value: 0,
+        name: "MATCH_START",         
+        image: "/img/png/128/Game-Start.png",
+        title: "Матч начался"
+    },
+    MATCH_END: {
+        value: 1,
+        name: "MATCH_END",
+        title: "Матч закончился"
+    },
+    TIME_START : {
+        value: 2,
+        name: "TIME_START"
+    },
+    TIME_END : {
+        value: 3,
+        name: "TIME_END"
+    },
+    GOAL : {
+        value: 4,
+        name: "GOAL",
+        image: "/img/png/128/Goal.png",
+        title: "Гол"
+    },
+    OWN_GOAL : {
+        value: 5,
+        name: "OWN_GOAL"
+    },
+    YELLOW_CARD : {
+        value: 6,
+        name: "YELLOW_CARD",
+        title: "Желтая карточка"
+    },
+    RED_CARD : {
+        value: 7,
+        name: "RED_CARD",
+        title: "Красная карточка"
+    }
+};
