28d8716bf5e59dc32caf76e3f856ecaa78333911
Ilya Petukhov <iproha94@gmail.com>
2017-02-16 20:34:41 +0300

    set default logo for team on tournament page

--- public/js/components/tournament/TeamsList.js
+++ public/js/components/tournament/TeamsList.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router';
+import TeamCard from './TeamCard';
 
 export default React.createClass({
     render: function () {
         var list = this.props.list.map((item) => {
-            let logo = `/uploaded/team/logo/${item._id}.png`;
             return (
-                <div className="col s12 l3 m4 list_team"  key={item._id}>
-                    <div className="card">
-                        <div className="list_team_content__padding">
-                            <div className="card-image team-logo-in-list">
-                                <img className="team-logo-in-list_image" src={logo} />
-                            </div>
-                            <div className="card-action">
-                                <Link to={"/team/" + item._id}>{item.name}</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <TeamCard logo = {`/uploaded/team/logo/${item._id}.png`}
+                          name = {item.name}
+                          id = {item._id}/>
             )
         });
         return (
